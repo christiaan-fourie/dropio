2a50e13003b91f9e048bcc8d6664c5b486d5ab36
Christiaan Fourie <christiaanfourie60@gmail.com>
2026-04-22 14:35:42 +0200

    fix /page issue with Next file structure.

--- app/lib/pdf/index.js
+++ app/lib/pdf/index.js
@@ -1,5 +1,5 @@
 export { generateBusinessCardsPDF } from "./businessCards";
 export { generateCustomLayoutPDF } from "./customLayout";
 export { generateCanvasWrapPDF } from "./canvasWrap";
-export { generatePagePDF } from "./page";
+export { generatePagePDF } from "./pageTool";
 export { downloadPdfBytes } from "./workers";
